f5ce6a3b6697e6951d8463ed6c63321efd288f27
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-09-19 20:01:19 -0300

    header estilizaçao

--- focal-point/src/app/layout.js
+++ focal-point/src/app/layout.js
@@ -1,5 +1,5 @@
 
-import Inter from 'next/font/google'
+import { Inter } from '@next/font/google'
 
 const interTight = Inter({
   subsets: ['latin'],
--- focal-point/src/app/page.js
+++ focal-point/src/app/page.js
@@ -1,19 +1,24 @@
 import '../styles/globals.scss'
 import Image from 'next/image';
 import logo from '../public/images/logo.svg'
+import styles from '../styles/Home.module.scss'
 
 
 export default function Home() {
   return (
     <div>
-        <header>
+        <header className={styles.headerHome}>
             <Image src={logo} 
             alt='Logo'  
-            width={150} 
-            height={150}
+            className={styles.logoHome}
             />
-            
+
+            <p className={styles.titleHome}>Bem-vindo de volta, Marcus</p>
+
+            <p className={styles.descricaoHome}>Segunda, 01 de dezembro de 2025</p>
+        
         </header>
+        <hr className={styles.linhaHome}></hr>
     </div>
   );
 }
