7ff23aa82d07ba1f66a527eb5ca18230cad38a66
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-09-20 10:11:51 -0300

    ajustes no checkbox das tarefas

--- focal-point/src/app/componentes/tarefas/tarefas.js
+++ focal-point/src/app/componentes/tarefas/tarefas.js
@@ -1,29 +1,74 @@
-import styles from '../tarefas/tarefas.module.scss'
-import Trash from '../../../public/images/trash.svg'
-import Image from 'next/image'
+import styles from "../tarefas/tarefas.module.scss";
+import Trash from "../../../public/images/trash.svg";
+import Image from "next/image";
 
-export default function Tarefas(){
-    return(
-        <div className={styles.mainContainer}>
-        <div className={styles.tarefasMain}>     
-            <p>Suas tarefas de hoje</p>
-            <div className={styles.CheckTarefa}>
-                <input type='checkbox' className={styles.inputTarefas}></input>
-                <label className={styles.nomesTarefas}>Fazer um bolo</label>
-                <Image src={Trash} alt="Icon de um lixo" className={styles.iconsTarefas} />
+export default function Tarefas() {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.tarefasMain}>
+        <p className={styles.titleTarefas}>Suas tarefas de hoje</p>
+
+        <div className={styles.sessaoTarefas}>
+          <div className={styles.CheckTarefa}>
+            <div className={styles.checkbox}>
+              <input
+                type="checkbox"
+                id="chk1"
+                className={styles.checkboxInput}
+              />
+              <label htmlFor="chk1">Fazer um bolo</label>
             </div>
-            <div className={styles.CheckTarefa}>
-                <input type='checkbox' className={styles.inputTarefas}></input>
-                <label className={styles.nomesTarefas}>Fazer um bolo</label>
-                <Image src={Trash} alt="Icon de um lixo" className={styles.iconsTarefas} />
+            <Image
+              src={Trash}
+              alt="Icon de um lixo"
+              className={styles.iconsTarefas}
+            />
+          </div>
+          <div className={styles.CheckTarefa}>
+            <div className={styles.checkbox}>
+              <input
+                type="checkbox"
+                id="chk2"
+                className={styles.checkboxInput}
+              />
+              <label htmlFor="chk2">Fazer um bolo</label>
             </div>
-            <div className={styles.CheckTarefa}>
-                <input type='checkbox' className={styles.inputTarefas}></input>
-                <label className={styles.nomesTarefas}>Fazer um bolo</label>
-                <Image src={Trash} alt="Icon de um lixo" className={styles.iconsTarefas} />
+            <Image
+              src={Trash}
+              alt="Icon de um lixo"
+              className={styles.iconsTarefas}
+            />
+          </div>
+          <div className={styles.CheckTarefa}>
+            <div className={styles.checkbox}>
+              <input
+                type="checkbox"
+                id="chk3"
+                className={styles.checkboxInput}
+              />
+              <label htmlFor="chk3">Fazer um bolo</label>
             </div>
+            <Image
+              src={Trash}
+              alt="Icon de um lixo"
+              className={styles.iconsTarefas}
+            />
+          </div>
         </div>
-    </div>
-    )
+        <p className={styles.titleTarefas}>Tarefas finalizadas</p>
 
+        <div className={styles.sessaoTarefasFinalizadas}>
+          <div className={styles.CheckTarefaFinalizada}>
+            <input type="checkbox" className={styles.inputTarefas}></input>
+            <label className={styles.nomesTarefas}>Fazer um bolo</label>
+            <Image
+              src={Trash}
+              alt="Icon de um lixo"
+              className={styles.iconsTarefas}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
